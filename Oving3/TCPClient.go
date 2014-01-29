2dd11cdf55bc3a5b690b272c8b95d9729c19a32e
student <student@Sanntid.(none)>
2014-01-29 11:33:13 +0100

    Fikset server og greier

--- Oving3/TCPClient.go
+++ Oving3/TCPClient.go
@@ -10,62 +10,71 @@ import (
 //Avbryter programmet og printer error-melding fra den aktuelle funksjonen
 func checkError(err error) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		fmt.Fprintf(os.Stderr, "Fatal error: %s\n", err.Error())
 		os.Exit(1)
 	}
 }
 
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+	for {	
+		buf := make([]byte, 1024)
+		n, err := conn.Read(buf[0:])
+		if err != nil {
+			return
+		}
+		_, err2 := conn.Write(buf[0:n])
+		if err2 != nil {
+			return
+		}
+	}
+}
+
+
 func main() {
 	
+	service := "127.0.0.1:12000"
+	serverAddr := "127.0.0.1:12345"
 	//serverAddr := "129.241.187.161:34933"
+	//port 34933 for message size 1024, port 33546 for terminering med \x00
 
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", "129.241.187.161:33546")
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", serverAddr)
 	checkError(err)
 
 	conn, err := net.DialTCP("tcp", nil, tcpAddr)
-
-	checkError(err)
-	data := make([]byte, 512)
-	_, err = conn.Read(data)
-	dataIn := string(data[:])
-	fmt.Println(dataIn)
-
-	b := make([]byte, 1024)
-	conn.Write([]byte("hallasldasdasdasdasdasdadssd\x00"))
-	conn.Read(b)
-	fmt.Printf("%s", b)
-	conn.Write([]byte("en ny beskjed\x00"))
-	conn.Read(b)
-	fmt.Printf("%s", b)
-
 	checkError(err)
 
-	//
-	tcpServer, err := net.ResolveTCPAddr("tcp", "129.241.187.140:33443")
-	checkError(err)
 
-	listener, err := net.ListenTCP("tcp", tcpServer)
-	conn.Write([]byte("connect to: 129.241.187.140:33443"))
-	conn.Read(b)
-	fmt.Println("%s", b)
+	conn.Write([]byte(service))
 	
+	listenAddr, _ := net.ResolveTCPAddr("tcp4", service)
+	listener, _ := net.ListenTCP("tcp", listenAddr)
 
-	go func(){
-		for {
-			connn,err := listener.Accept()
-			if err != nil {
-				continue
-			}
-			connn.Write([]byte("ahllow hal\x00"))
-			connn.Read(b)
-			fmt.Printf("%s", b)
-			
+	rcv := make([]byte, 1024)
+	connRcv, _ := listener.Accept()
+	connRcv.Read(rcv)
+
+	fmt.Printf("%s", rcv)
+
+	//b := make([]byte, 1024)
+	connRcv.Write([]byte("Beskjed 1: 12345678910 TestTestTest\x00\n"))
+	//connRcv.Read(b)
+	//fmt.Printf("%s", b)
+
+	for {
+		buf := make([]byte, 1024)
+		n, err := connRcv.Read(buf[0:])
+		fmt.Printf("%s", buf)
+		if err != nil {
+			return
 		}
-	}()
-	
+		_, err2 := connRcv.Write(buf[0:n])
+		if err2 != nil {
+			return
+		}
+	}
 	
 
-
+	fmt.Println("end")
 	os.Exit(0)
-	
 }
--- Oving3/TCPServer.go
+++ Oving3/TCPServer.go
@@ -1,4 +1,4 @@
-	package main
+package main
 
 import (
 	"fmt"
@@ -6,6 +6,40 @@ import (
 	"os"
 )
 
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s\n", err.Error())
+		os.Exit(1)
+	}
+}
+
 func main() {
+
+	//port 34933 for message size 1024, port 33546 for terminering med \x00
+	service := "127.0.0.1:12345"
+	tcpAddr, _ := net.ResolveTCPAddr("tcp4", service)
+	listener, _ := net.ListenTCP("tcp", tcpAddr)
+
+	conn, _ := listener.Accept()
+	buf := make([]byte, 1024)
+	conn.Read(buf)
+
+	tcpAddr2, _ := net.ResolveTCPAddr("tcp4", string(buf))
+	conn2, _ := net.DialTCP("tcp", nil, tcpAddr2)
+	conn2.Write([]byte("Hello!\n"))
+	//fmt.Printf("%s", buf)
 	
+	for {
+		n, err1 := conn2.Read(buf[0:])
+		if err1 != nil {
+			return
+		}
+		_, err2 := conn2.Write(buf[0:n])
+		if err2 != nil {
+			return
+		}
+	}
+	conn.Close()
+	fmt.Println("end")
+	os.Exit(0)	
 }
